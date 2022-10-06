21dde1c9f95b5499b7d3fa6699faa6ef7d0e1f2a
Rose Khaldeeva <khalrose@mail.ru>
2022-10-06 16:18:22 +0600

    Set up WelcomeViewController

--- AboutMeApp/ViewControllers/WelcomeViewController.swift
+++ AboutMeApp/ViewControllers/WelcomeViewController.swift
@@ -9,24 +9,26 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    @IBOutlet weak var welcomeLabel: UILabel!
     
+    var user = ""
     
+    private let primaryColor = UIColor(
+        red: 210/255,
+        green: 109/255,
+        blue: 128/255,
+        alpha: 1
+    )
+    private let secondaryColor = UIColor(
+        red: 107/255,
+        green: 148/255,
+        blue: 230/255,
+        alpha: 1
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        welcomeLabel.text ="Welcome, \(user)!"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
